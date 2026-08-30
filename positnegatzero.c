c8ef67f326686e8878fd3078b467052c93ea0201
tanvi-bhandari19 <tanvi.37162@stu.upes.ac.in>
2026-08-30 13:23:15 +0530

    Add files via upload
    To check whether an integer is positive, negative or zero using nested if-else

---
+++ positnegatzero.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter an integer: ");
+scanf("%d",&num);
+if (num>=0)
+{
+if (num == 0)
+printf("The number is Zr=ero\n");
+else
+printf("The number is Positive\n");
+}
+else
+{
+printf("The number is Negative\n");
+}
+return 0;
+}
