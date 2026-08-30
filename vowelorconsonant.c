25d8ce1147aaebf2ea78dcb44c27d1104c96e5ca
tanvi-bhandari19 <tanvi.37162@stu.upes.ac.in>
2026-08-30 13:44:06 +0530

    Add files via upload
    To input a character and check whether it is a vowel or consonant using if-else

---
+++ vowelorconsonant.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("Enter a character: ");
+scanf("%c",&ch);
+if((ch>='A'&& ch<='Z'||ch>='a' && ch<='z'))
+if
+(ch=='a'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'|| ch=='U')
+{
+printf("Vowel");
+}
+else
+{
+printf("Consonant\n");
+}
+return 0;
+}
