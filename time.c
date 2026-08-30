54fe21a7c1ba78d233e141de57a8d0a6aea87405
tanvi-bhandari19 <tanvi.37162@stu.upes.ac.in>
2026-08-30 13:08:02 +0530

    Add files via upload
    Program to enter time in seconds and convert it into hours:minutes:seconds format

---
+++ time.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int seconds,hours,minutes;
+printf("Enter time in seconds: ");
+scanf("%d",&seconds);
+hours=seconds/3600;
+seconds=seconds%3600;
+minutes=seconds/60;
+seconds=seconds%60;
+printf("Time = %d hours : %d minutes : %d seconds\n", hours, minutes, seconds);
+return 0;
+}
